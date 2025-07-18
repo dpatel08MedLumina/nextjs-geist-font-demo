cf93baeb015abef60c66e374756f9294036f6c0b
dpatel08MedLumina <dev2027patel@gmail.com>
2025-07-17 19:27:44 -0500

    Add .next/static/chunks/src_app_project-collaboration_page_tsx_0ef0cac3._.js 1752798464267

---
+++ .next/static/chunks/src_app_project-collaboration_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a1aba6eb._.js",
+  "static/chunks/node_modules_5414f1ce._.js"
+],
+    source: "dynamic"
+});
