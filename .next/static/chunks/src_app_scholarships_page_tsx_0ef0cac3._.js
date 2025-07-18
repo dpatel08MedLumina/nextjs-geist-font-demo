6ea0a8961e994a6380d44ac01f959ce37a05a141
dpatel08MedLumina <dev2027patel@gmail.com>
2025-07-17 19:27:39 -0500

    Add .next/static/chunks/src_app_scholarships_page_tsx_0ef0cac3._.js 1752798459155

---
+++ .next/static/chunks/src_app_scholarships_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ae1c6d9b._.js",
+  "static/chunks/node_modules_dd28a197._.js"
+],
+    source: "dynamic"
+});
